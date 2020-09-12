4bf7a6f38fa23dc4fb21e0e1379d1aaf31c101f5
veysi <veysi6188@gmail.com>
2020-09-12 16:28:43 +0300

    first commit

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
-export const API_BASE = "http://localhost:3004"
-export const fbapiKey= "AIzaSyAyfmQdXZ2pJOwmtmoCGlNUGoisab9ihSM"
+
+export const fbapiKey= "Firebase Api key"
 export const fbSignIn= `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${fbapiKey}`
 export const fbSignUp= `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${fbapiKey}`
